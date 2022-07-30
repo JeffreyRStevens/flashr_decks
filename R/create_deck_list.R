c8c3094a26cb3b4924fb52c4bc86cb1b72ffd368
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2022-07-30 11:47:34 -0500

    Create create_deck_list.R

---
+++ R/create_deck_list.R
@@ -0,0 +1,42 @@
+
+library(flashr)
+library(here)
+
+# Get file names in repository
+get_repo <- function(repo_text) {
+  gh::gh(repo_text) |>
+    vapply("[[", "", "name")
+}
+get_repo_mem <- memoise::memoise(get_repo)
+
+# Extract title from CSV
+get_title <- function(x) {
+  data <- utils::read.csv(x)
+  data$title[1]
+}
+get_title_mem <- memoise::memoise(get_title)
+
+repo <- "JeffreyRStevens/flashr_decks"
+repo_text <- paste0("GET /repos/", repo, "/contents/decks")
+deckfiles <- get_repo_mem(repo_text)
+deckfiles <- deckfiles[which(deckfiles != "00_all_decks.csv")]
+
+# Create labels, paths, and titles for decks
+deckpaths <-
+  paste0("https://raw.githubusercontent.com/", repo, "/main/decks/", deckfiles)
+decklabels <- gsub(".csv", "", deckfiles)
+titles <- vapply(deckpaths, get_title_mem, character(1))
+decks <- paste0(titles, " (", decklabels, ")") |>
+  gtools::mixedsort()
+
+all_decks <- data.frame(deck = decks, title = titles, decklabel = decklabels)
+
+write.csv(all_decks, here("decks/00_all_decks.csv"), row.names = FALSE)
+
+
+# Create HTML decks for website ------------------------------------------------
+
+for (i in seq_along(all_decks$decklabel)) {
+  deck <- all_decks$decklabel[i]
+  flashcard(deck, file = here(paste0("_site/decks/", deck, ".html")))
+}
